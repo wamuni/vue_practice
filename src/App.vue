1c4915f4d9b088710681c154a823834ff7c47768
wamuni <boshi.pan@outlook.com>
2024-04-30 22:20:43 +0100

    1. remove all original files created from cli 2. add new main.ts to understand how exactly createApp function works 3. add new component 'Person' to modulize the project 4. setup function in vue3, use it as import and export for variables, components, etc 5. use normal <script> tag to export component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="box">
+        <PersonComponent />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script lang="ts">
+export default {
+    name: 'App'
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<script setup lang="ts">
+import PersonComponent from './components/Person.vue'
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+.box {
+    background-color: burlywood;
+    border-radius: 10px;
+    padding: 10px;
+    box-shadow: 0 0 10px;
 }
 </style>
---
+++ src/components/Person.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="person">
+        <h2>Name: {{ name }}</h2>
+        <h4>Age: {{ age }}</h4>
+        <h4>Address: {{ address }}</h4>
+        <button @click="changeName">Change Name</button>
+        <button @click="changeAge">Change Age</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+    name: 'PersonComponent'
+}
+</script>
+
+<script setup lang="ts">
+let name = ref('Eddie')
+let age = ref(18)
+let address = 'Ardpatrick Louth Village'
+
+function changeName() {
+    name.value = 'Eddiddie'
+}
+
+function changeAge() {
+    age.value += 1
+}
+</script>
+
+<style scoped>
+.person {
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    background-color: cadetblue;
+    padding: 10px;
+}
+
+button {
+    margin: 10px;
+    padding: 10px;
+    border-radius: 5px;
+    background-color: chocolate;
+}
+</style>
